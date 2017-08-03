1e6e36d652aaaa8b2685c829fc4fee6adcb367d3
dlowder-salesforce <dlowder@salesforce.com>
2017-08-03 14:55:44 -0700

    Go to Cocoapods model and remove unneeded files

---
+++ tvos/VictoryTest/VictoryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VictoryTest
+//
+//  Created by Douglas Lowder on 8/3/17.
+//  Copyright © 2017 Douglas Lowder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tvos/VictoryTest/VictoryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VictoryTest
+//
+//  Created by Douglas Lowder on 8/3/17.
+//  Copyright © 2017 Douglas Lowder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
